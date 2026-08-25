7fb30b57c422618f5a5397ba1c348abed1a02534
dengzhen <dengzhen@zhongshitech.cn>
2026-08-25 17:10:15 +0800

    chore: release Vinea 0.3.0

--- plugins/vinea/bin/vinea.mjs
+++ plugins/vinea/bin/vinea.mjs
@@ -5658,7 +5658,7 @@ var init_workflow = __esm({
 // package.json
 var package_default = {
   name: "vinea",
-  version: "0.2.0",
+  version: "0.3.0",
   private: true,
   type: "module",
   engines: {
